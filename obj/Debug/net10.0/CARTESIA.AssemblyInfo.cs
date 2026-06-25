51e07bc1abeea71aa96dbe9328849f2e282f2883
Roberto Jerez <122759985+rojzzzzz@users.noreply.github.com>
2026-06-24 21:03:22 -0600

    cache

--- obj/Debug/net10.0/CARTESIA.AssemblyInfo.cs
+++ obj/Debug/net10.0/CARTESIA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CARTESIA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70612b1055b17d47118f8098b1ba7d83d825a0a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745280d4a6672d9a34180aa8c759f4270cc23310")]
 [assembly: System.Reflection.AssemblyProductAttribute("CARTESIA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CARTESIA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
